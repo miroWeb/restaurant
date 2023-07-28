0b14cc8d3f9786c5b82128e0a00314b5aeccb596
miroWeb <mirodiljs2004@gmail.com>
2023-07-28 15:07:52 +0500

    aa

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 function App() {
   return (
     <div>
-      <h1>salom Alik alik</h1>
+      <h1>salom Alik alik
+        DEWJHIDEWFJEWOIF
+      </h1>
     </div>
   )
 }
